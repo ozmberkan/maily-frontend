2a32cb32b7bb1c4ca17b624f6b41208d52fe3ced
ozmberkan <ozmberkan@gmail.com>
2025-01-14 23:58:15 +0300

    layout yapısı tamamlandı.

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div>Home</div>;
+  return <div className=" bg-blue-500 w-full h-full">Dashboard</div>;
 }
---
+++ components/Sidebar/Sidebar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Sidebar = () => {
+  return <div className="min-w-64 max-w-64 h-full bg-orange-500">Sidebar</div>;
+};
+
+export default Sidebar;
