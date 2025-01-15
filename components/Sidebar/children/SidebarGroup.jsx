c8548ef98ebf2b48517d4ae7353c5d1250b6f81c
ozmberkan <ozmberkan@gmail.com>
2025-01-15 22:49:37 +0300

    context eklendi.

--- components/Sidebar/children/SidebarGroup.jsx
+++ components/Sidebar/children/SidebarGroup.jsx
@@ -1,5 +1,11 @@
 import Link from "next/link";
 import React from "react";
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuTrigger,
+} from "@/components/ui/context-menu";
 
 const SidebarGroup = ({ title, links }) => {
   return (
@@ -7,14 +13,37 @@ const SidebarGroup = ({ title, links }) => {
       <h1 className="text-xs font-medium text-neutral-400">{title}</h1>
       <ul className="flex flex-col ">
         {links.map((link) => (
-          <Link
-            key={link.id}
-            href={link.to}
-            className="flex items-center hover:bg-neutral-100 p-2 text-neutral-500 hover:text-neutral-600 rounded-md gap-x-2"
-          >
-            <link.icon />
-            <span className="text-xs  font-medium">{link.label}</span>
-          </Link>
+          <div key={link.id}>
+            {link.isContext ? (
+              <ContextMenu>
+                <ContextMenuTrigger>
+                  <Link
+                    className="flex items-center hover:bg-neutral-100 p-2 text-neutral-500 hover:text-neutral-600 rounded-md gap-x-2"
+                    href={link.to}
+                  >
+                    <link.icon />
+                    <span className="text-xs  font-medium">{link.label}</span>
+                  </Link>
+                </ContextMenuTrigger>
+                <ContextMenuContent>
+                  <ContextMenuItem asChild>
+                    <button className="text-xs w-full cursor-pointer">
+                      Klasör oluştur
+                    </button>
+                  </ContextMenuItem>
+                </ContextMenuContent>
+              </ContextMenu>
+            ) : (
+              <Link
+                key={link.id}
+                href={link.to}
+                className="flex items-center hover:bg-neutral-100 p-2 text-neutral-500 hover:text-neutral-600 rounded-md gap-x-2"
+              >
+                <link.icon />
+                <span className="text-xs  font-medium">{link.label}</span>
+              </Link>
+            )}
+          </div>
         ))}
       </ul>
     </div>
--- constants/data.js
+++ constants/data.js
@@ -3,11 +3,23 @@ import { FiFile, FiHome, FiInbox, FiMail, FiTrash } from "react-icons/fi";
 import { HiHashtag } from "react-icons/hi";
 
 export const firstLinks = [
-  { id: 1, icon: FiHome, to: "/", label: "Anasayfa" },
-  { id: 2, icon: FiInbox, to: "/inbox", label: "Gelen Kutusu" },
-  { id: 3, icon: FiMail, to: "/sent-mails", label: "Gönderilen Postalar" },
-  { id: 4, icon: FiFile, to: "/draft", label: "Taslak" },
-  { id: 5, icon: FiTrash, to: "/trash", label: "Çöp Kutusu" },
+  { id: 1, icon: FiHome, to: "/", label: "Anasayfa", isContext: false },
+  {
+    id: 2,
+    icon: FiInbox,
+    to: "/inbox",
+    label: "Gelen Kutusu",
+    isContext: true,
+  },
+  {
+    id: 3,
+    icon: FiMail,
+    to: "/sent-mails",
+    label: "Gönderilen Postalar",
+    isContext: true,
+  },
+  { id: 4, icon: FiFile, to: "/draft", label: "Taslak", isContext: false },
+  { id: 5, icon: FiTrash, to: "/trash", label: "Çöp Kutusu", isContext: false },
 ];
 
 export const personalLinks = [
