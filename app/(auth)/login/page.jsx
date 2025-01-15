77c65a4e0587a841d3b87e8501e8f78829154d09
ozmberkan <ozmberkan@gmail.com>
2025-01-15 19:19:25 +0300

    auth sayfaları tamamlandı / görsel için iyileştirme devam ediyor.

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,7 +1,47 @@
+import Link from "next/link";
 import React from "react";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div className="w-full flex items-start justify-start gap-5  h-screen p-5">
+      <div className="w-1/2 h-full rounded bg-loginHero bg-no-repeat bg-cover bg-center p-5"></div>
+      <div className=" w-1/2 h-full flex flex-col justify-start items-start p-24">
+        <div className="mb-2 pb-2 border-b text-2xl font-bold tracking-tighter bg-clip-text bg-gradient-to-l text-transparent from-sky-500 via-blue-500 to-red-500">
+          Maily
+        </div>
+        <form className="flex flex-col gap-3">
+          <div className="flex flex-col gap-2">
+            <h1 className="font-bold text-2xl text-neutral-700">Giriş Yap</h1>
+            <p className="text-xs font-medium text-neutral-500">
+              Hoş geldin, Bilgilerini doldurarak sisteme erişebilirsin.{" "}
+            </p>
+          </div>
+          <input
+            className="px-4 py-2 rounded border outline-none text-sm"
+            placeholder="Kullanıcı adı"
+          />
+          <input
+            className="px-4 py-2 rounded border outline-none text-sm"
+            placeholder="Parola"
+          />
+          <div className="w-full flex justify-between items-center">
+            <Link
+              href={"/register"}
+              className="text-xs text-neutral-600 hover:underline font-medium"
+            >
+              Hesabın yok mu?
+            </Link>
+          </div>
+          <button
+            type="submit"
+            className="bg-[#FC4652] text-sm text-white rounded py-2"
+          >
+            Oturum aç
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
--- app/(auth)/register/page.jsx
+++ app/(auth)/register/page.jsx
@@ -1,7 +1,52 @@
+import Logo from "@/components/Logo/Logo";
+import Link from "next/link";
 import React from "react";
 
 const RegisterPage = () => {
-  return <div>RegisterPage</div>;
+  return (
+    <div className="w-full flex items-start justify-start gap-5  h-screen p-5">
+      <div className="w-1/2 h-full rounded bg-loginHero bg-no-repeat bg-cover bg-center p-5"></div>
+      <div className=" w-1/2 h-full flex flex-col justify-start items-start p-24">
+        <div className="mb-2 pb-2 border-b text-2xl font-bold tracking-tighter bg-clip-text bg-gradient-to-l text-transparent from-sky-500 via-blue-500 to-red-500">
+          Maily
+        </div>
+        <form className="flex flex-col gap-3">
+          <div className="flex flex-col gap-2">
+            <h1 className="font-bold text-2xl text-neutral-700">Kayıt ol</h1>
+            <p className="text-xs font-medium text-neutral-500">
+              Hoş geldin, Bilgilerini doldurarak sisteme kayıt olabilirsin.{" "}
+            </p>
+          </div>
+          <input
+            className="px-4 py-2 rounded border outline-none text-sm"
+            placeholder="İsim Soyisim"
+          />
+          <input
+            className="px-4 py-2 rounded border outline-none text-sm"
+            placeholder="Kullanıcı adı"
+          />
+          <input
+            className="px-4 py-2 rounded border outline-none text-sm"
+            placeholder="Parola"
+          />
+          <div className="w-full flex justify-between items-center">
+            <Link
+              href={"/login"}
+              className="text-xs text-neutral-600 hover:underline font-medium"
+            >
+              Hesabın var mı?
+            </Link>
+          </div>
+          <button
+            type="submit"
+            className="bg-[#FC4652] text-sm text-white rounded py-2"
+          >
+            Kayıt ol
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default RegisterPage;
